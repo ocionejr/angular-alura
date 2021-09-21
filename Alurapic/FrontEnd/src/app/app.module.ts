4a8538b6556c394f0ba3a1ffb180cab149d9249f
ocionejr <ocionejunior@gmail.com>
2021-09-21 10:07:51 -0300

    Parte 1, módulo 6

--- Alurapic/FrontEnd/src/app/app.module.ts
+++ Alurapic/FrontEnd/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { PhotosModule } from "./photos/photos.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule],
+  imports: [BrowserModule, PhotosModule, AppRoutingModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
