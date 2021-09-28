865c6c456f778365d2605344f51740b504eb6935
ocionejr <ocionejunior@gmail.com>
2021-09-28 13:49:55 -0300

    Parte 4 módulo 5 concluído

--- FrontEnd/src/app/errors/errors.module.ts
+++ FrontEnd/src/app/errors/errors.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
+import { GlobalErrorComponent } from './global-error/global-error.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [NotFoundComponent]
+  imports: [CommonModule, RouterModule],
+  declarations: [NotFoundComponent, GlobalErrorComponent],
+  providers: [{ provide: ErrorHandler, useClass: GlobalErrorHandler }],
 })
-export class ErrorsModule { }
+export class ErrorsModule {}
